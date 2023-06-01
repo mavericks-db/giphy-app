621daa10215fac27e32407f587a372aef7c97e8b
Mavericks Balitaan <balitaanmavericks@gmail.com>
2023-06-01 07:45:09 +0500

    Add style

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const gifContainer = document.querySelector("#gif-container");
 const searchBoxEl = document.querySelector("#search-box");
 const searchButtonEl = document.querySelector("#search-button");
 
-const apiURL = "https://api.giphy.com/v1/gifs/search?api_key=WW8ndXrgcWP3aru6FW3Ts3T0tq1HTr9A&limit=10&rating=g&q=";
+const apiURL = "https://api.giphy.com/v1/gifs/search?api_key=WW8ndXrgcWP3aru6FW3Ts3T0tq1HTr9A&limit=12&rating=g&q=";
 
 function getGifs() {
+  gifContainer.innerHTML = "";
   let searchTerm = searchBoxEl.value;
-  console.log(searchTerm);
 
   fetch(apiURL + searchTerm).then(res => res.json()).then(data => {
     const results = data.data;
@@ -14,12 +14,31 @@ function getGifs() {
 
     results.map(gif => {
       let giphyBox = document.createElement("div");
-      let title = document.createElement("h4");
-
+      let link = document.createElement("a");
+      let title = document.createElement("h5");
+      let image = document.createElement("img");
+      giphyBox.classList.add("giphyBox");
+      image.src = gif.images.original.url;
+      image.classList.add("gif-image");
+      link.href = gif.embed_url;
+      link.target = "_blank";
       title.textContent = gif.title;
-      giphyBox.innerHTML = `<img src="${gif.images.original.url}" alt="${gif.title}" />`
 
-      gifContainer.append(title, giphyBox);
+      link.appendChild(title);
+      giphyBox.append(link, image);
+      gifContainer.appendChild(giphyBox);
     })
   })
+
+  searchBoxEl.value = "";
 }
+
+function enter(e) {
+  if (e.keyCode === 13) {
+    getGifs();
+  }
+}
+
+window.addEventListener("keypress", (e) => {
+  enter(e);
+})
